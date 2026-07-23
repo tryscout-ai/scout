59fb42e12704ec00b97c428a55a4720626a8ad20
sahil_tiwari <soulsahil200214@gmail.com>
2026-07-23 16:10:21 +0530

    fix: debugging middleware

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -2,6 +2,7 @@ import { type NextRequest } from "next/server";
 import { updateSession } from "@/lib/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
+  console.log("MIDDLEWARE:", request.nextUrl.pathname);
   return await updateSession(request);
 }
 
